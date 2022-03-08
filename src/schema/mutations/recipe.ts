cae93c9935a8153c977740ca09dc41ed69288724
he1d1 <email@lewie.me>
2022-03-08 14:31:30 +0000

    Added image uploading

--- src/schema/mutations/recipe.ts
+++ src/schema/mutations/recipe.ts
@@ -1,10 +1,12 @@
 import { prisma } from "../../prisma";
+import { s3 } from "../../app";
 
 export default {
   createRecipe: async (
     _ = null,
     {
       recipe,
+      image,
     }: {
       recipe: {
         name: string;
@@ -13,6 +15,9 @@ export default {
         cuisine?: string;
         cookTime?: number;
         prepTime?: number;
+      };
+      image: {
+        base64: string;
       };
     },
     args: { id: string }
@@ -25,23 +30,135 @@ export default {
       };
     }
 
+    // Image buffer stores the decoded base64 image.
+    let imageBuffer;
+    // Check if there is an image
+    if (image?.base64) {
+      // Check if the image is a valid base64 string
+      try {
+        imageBuffer = Buffer.from(image.base64.split(",")[1], "base64");
+        if (imageBuffer.length > 1000000) {
+          return {
+            code: 400,
+            message: "Image is too large",
+          };
+        }
+      } catch (e) {
+        return {
+          code: 400,
+          message: "Image is not a valid base64 string",
+        };
+      }
+
+      // Check the image file type
+      const imageType = image.base64.split(";")[0].split("/")[1];
+      if (!["jpeg", "png", "jpg"].includes(imageType)) {
+        return {
+          code: 400,
+          message: "Image type is not supported",
+        };
+      }
+
+      // Check if the image is a valid image
+      // Image is valid if it starts with data:image/
+      // then has a file type of jpeg, png, or jpg
+      // then has a semicolon
+      // then says base64
+      // then has a comma
+
+      if (imageBuffer.toString().match(/^data:image\/[a-zA-Z]+;base64,/)) {
+        return {
+          code: 400,
+          message: "Image is not a valid image",
+        };
+      }
+    }
+
     // Create recipe
-    return await prisma.recipe.create({
-      data: {
-        ...recipe,
-        author: {
-          connect: {
-            id: args.id,
+    if (!imageBuffer) {
+      return await prisma.recipe.create({
+        data: {
+          ...recipe,
+          author: {
+            connect: {
+              id: args.id,
+            },
           },
         },
-      },
-    });
+      });
+    } else {
+      // generate id for recipe
+      const { id } = await prisma.recipe.create({
+        data: {
+          ...recipe,
+          author: {
+            connect: {
+              id: args.id,
+            },
+          },
+        },
+      });
+      // Upload image to S3
+      const imageName = `${args.id}/${id}/cover.${
+        image.base64.toString().split(";")[0].split("/")[1]
+      }`;
+
+      try {
+        s3.putObject(
+          {
+            Bucket: process.env.AWS_S3_BUCKET ?? "",
+            Key: imageName,
+            Body: imageBuffer,
+            ContentEncoding: "base64",
+            ContentType: image.base64.split(";")[0],
+          },
+          (err, data) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+      } catch (error) {
+        // Delete user if image upload fails
+        await prisma.recipe.delete({
+          where: {
+            id,
+          },
+        });
+
+        console.error(error);
+
+        return {
+          code: 500,
+          message: "Error uploading image to S3",
+        };
+      }
+      return await prisma.recipe.update({
+        where: {
+          id,
+        },
+        data: {
+          image: {
+            create: {
+              url:
+                (process.env.CDN_URL ??
+                  `https://${process.env.AWS_S3_BUCKET ?? ""}.s3.${
+                    process.env.AWS_REGION
+                  }.amazonaws.com`) +
+                "/" +
+                imageName,
+            },
+          },
+        },
+      });
+    }
   },
   editRecipe: async (
     _ = null,
     {
       id,
       recipe,
+      image,
     }: {
       id: string;
       recipe: {
@@ -51,6 +168,9 @@ export default {
         cuisine?: string;
         cookTime?: number;
         prepTime?: number;
+      };
+      image: {
+        base64: string;
       };
     },
     args: { id: string }
@@ -84,15 +204,119 @@ export default {
       };
     }
 
+    // Image buffer stores the decoded base64 image.
+    let imageBuffer;
+    // Check if there is an image
+    if (image?.base64) {
+      // Check if the image is a valid base64 string
+      try {
+        imageBuffer = Buffer.from(image.base64.split(",")[1], "base64");
+        if (imageBuffer.length > 1000000) {
+          return {
+            code: 400,
+            message: "Image is too large",
+          };
+        }
+      } catch (e) {
+        return {
+          code: 400,
+          message: "Image is not a valid base64 string",
+        };
+      }
+
+      // Check the image file type
+      const imageType = image.base64.split(";")[0].split("/")[1];
+      if (!["jpeg", "png", "jpg"].includes(imageType)) {
+        return {
+          code: 400,
+          message: "Image type is not supported",
+        };
+      }
+
+      // Check if the image is a valid image
+      // Image is valid if it starts with data:image/
+      // then has a file type of jpeg, png, or jpg
+      // then has a semicolon
+      // then says base64
+      // then has a comma
+
+      if (imageBuffer.toString().match(/^data:image\/[a-zA-Z]+;base64,/)) {
+        return {
+          code: 400,
+          message: "Image is not a valid image",
+        };
+      }
+    }
+
     // Update recipe
-    return await prisma.recipe.update({
-      where: {
-        id,
-      },
-      data: {
-        ...recipe,
-      },
-    });
+    if (!imageBuffer) {
+      return await prisma.recipe.update({
+        where: {
+          id,
+        },
+        data: {
+          ...recipe,
+        },
+      });
+    } else {
+      // Upload image to S3
+      const imageName = `${args.id}/${id}/cover.${
+        image.base64.toString().split(";")[0].split("/")[1]
+      }`;
+
+      try {
+        s3.putObject(
+          {
+            Bucket: process.env.AWS_S3_BUCKET ?? "",
+            Key: imageName,
+            Body: imageBuffer,
+            ContentEncoding: "base64",
+            ContentType: image.base64.split(";")[0],
+          },
+          (err, data) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+      } catch (error) {
+        console.error(error);
+
+        return {
+          code: 500,
+          message: "Error uploading image to S3",
+        };
+      }
+      return await prisma.recipe.update({
+        where: {
+          id,
+        },
+        data: {
+          image: {
+            upsert: {
+              create: {
+                url:
+                  (process.env.CDN_URL ??
+                    `https://${process.env.AWS_S3_BUCKET ?? ""}.s3.${
+                      process.env.AWS_REGION
+                    }.amazonaws.com`) +
+                  "/" +
+                  imageName,
+              },
+              update: {
+                url:
+                  (process.env.CDN_URL ??
+                    `https://${process.env.AWS_S3_BUCKET ?? ""}.s3.${
+                      process.env.AWS_REGION
+                    }.amazonaws.com`) +
+                  "/" +
+                  imageName,
+              },
+            },
+          },
+        },
+      });
+    }
   },
   deleteRecipe: async (
     _ = null,
@@ -131,7 +355,7 @@ export default {
       };
     }
 
-    // Update recipe
+    // Delete recipe
     return await prisma.recipe.delete({
       where: {
         id,
--- src/schema/mutations/step.ts
+++ src/schema/mutations/step.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../../prisma";
+import { s3 } from "../../app";
 
 export default {
   createStep: async (
@@ -6,7 +7,12 @@ export default {
     {
       recipe,
       step,
-    }: { recipe: string; step: { name?: string; content: string } },
+      image,
+    }: {
+      recipe: string;
+      step: { name?: string; content: string };
+      image: { base64: string };
+    },
     args: { id: string }
   ) => {
     // Check if user is authenticated
@@ -34,21 +40,140 @@ export default {
       };
     }
 
+    // Image buffer stores the decoded base64 image.
+    let imageBuffer;
+    // Check if there is an image
+    if (image?.base64) {
+      // Check if the image is a valid base64 string
+      try {
+        imageBuffer = Buffer.from(image.base64.split(",")[1], "base64");
+        if (imageBuffer.length > 1000000) {
+          return {
+            code: 400,
+            message: "Image is too large",
+          };
+        }
+      } catch (e) {
+        return {
+          code: 400,
+          message: "Image is not a valid base64 string",
+        };
+      }
+
+      // Check the image file type
+      const imageType = image.base64.split(";")[0].split("/")[1];
+      if (!["jpeg", "png", "jpg"].includes(imageType)) {
+        return {
+          code: 400,
+          message: "Image type is not supported",
+        };
+      }
+
+      // Check if the image is a valid image
+      // Image is valid if it starts with data:image/
+      // then has a file type of jpeg, png, or jpg
+      // then has a semicolon
+      // then says base64
+      // then has a comma
+
+      if (imageBuffer.toString().match(/^data:image\/[a-zA-Z]+;base64,/)) {
+        return {
+          code: 400,
+          message: "Image is not a valid image",
+        };
+      }
+    }
+
     // Create step
-    return await prisma.step.create({
-      data: {
-        ...step,
-        recipe: {
-          connect: {
-            id: recipe,
+    if (!imageBuffer) {
+      return await prisma.step.create({
+        data: {
+          ...step,
+          recipe: {
+            connect: {
+              id: recipe,
+            },
           },
         },
-      },
-    });
+      });
+    } else {
+      // generate id for step
+      const { id } = await prisma.step.create({
+        data: {
+          ...step,
+          recipe: {
+            connect: {
+              id: recipe,
+            },
+          },
+        },
+      });
+      // Upload image to S3
+      const imageName = `${args.id}/${recipe}/${id}.${
+        image.base64.toString().split(";")[0].split("/")[1]
+      }`;
+
+      try {
+        s3.putObject(
+          {
+            Bucket: process.env.AWS_S3_BUCKET ?? "",
+            Key: imageName,
+            Body: imageBuffer,
+            ContentEncoding: "base64",
+            ContentType: image.base64.split(";")[0],
+          },
+          (err, data) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+      } catch (error) {
+        // Delete user if image upload fails
+        await prisma.step.delete({
+          where: {
+            id,
+          },
+        });
+
+        console.error(error);
+
+        return {
+          code: 500,
+          message: "Error uploading image to S3",
+        };
+      }
+      return await prisma.step.update({
+        where: {
+          id,
+        },
+        data: {
+          image: {
+            create: {
+              url:
+                (process.env.CDN_URL ??
+                  `https://${process.env.AWS_S3_BUCKET ?? ""}.s3.${
+                    process.env.AWS_REGION
+                  }.amazonaws.com`) +
+                "/" +
+                imageName,
+            },
+          },
+        },
+      });
+    }
   },
   editStep: async (
     _ = null,
-    { id, step }: { id: string; step: { name?: string; content: string } },
+    {
+      id,
+      step,
+      image,
+    }: {
+      id: string;
+      step: { name?: string; content: string };
+      image: { base64: string };
+    },
     args: { id: string }
   ) => {
     // Check if user is authenticated
@@ -68,6 +193,7 @@ export default {
         recipe: {
           select: {
             authorId: true,
+            id: true,
           },
         },
       },
@@ -87,15 +213,119 @@ export default {
       };
     }
 
+    // Image buffer stores the decoded base64 image.
+    let imageBuffer;
+    // Check if there is an image
+    if (image?.base64) {
+      // Check if the image is a valid base64 string
+      try {
+        imageBuffer = Buffer.from(image.base64.split(",")[1], "base64");
+        if (imageBuffer.length > 1000000) {
+          return {
+            code: 400,
+            message: "Image is too large",
+          };
+        }
+      } catch (e) {
+        return {
+          code: 400,
+          message: "Image is not a valid base64 string",
+        };
+      }
+
+      // Check the image file type
+      const imageType = image.base64.split(";")[0].split("/")[1];
+      if (!["jpeg", "png", "jpg"].includes(imageType)) {
+        return {
+          code: 400,
+          message: "Image type is not supported",
+        };
+      }
+
+      // Check if the image is a valid image
+      // Image is valid if it starts with data:image/
+      // then has a file type of jpeg, png, or jpg
+      // then has a semicolon
+      // then says base64
+      // then has a comma
+
+      if (imageBuffer.toString().match(/^data:image\/[a-zA-Z]+;base64,/)) {
+        return {
+          code: 400,
+          message: "Image is not a valid image",
+        };
+      }
+    }
+
     // Update step
-    return await prisma.step.update({
-      where: {
-        id,
-      },
-      data: {
-        ...step,
-      },
-    });
+    if (!imageBuffer) {
+      return await prisma.step.update({
+        where: {
+          id,
+        },
+        data: {
+          ...step,
+        },
+      });
+    } else {
+      // Upload image to S3
+      const imageName = `${args.id}/${recipeExists.recipe.id}/${id}.${
+        image.base64.toString().split(";")[0].split("/")[1]
+      }`;
+
+      try {
+        s3.putObject(
+          {
+            Bucket: process.env.AWS_S3_BUCKET ?? "",
+            Key: imageName,
+            Body: imageBuffer,
+            ContentEncoding: "base64",
+            ContentType: image.base64.split(";")[0],
+          },
+          (err, data) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+      } catch (error) {
+        console.error(error);
+
+        return {
+          code: 500,
+          message: "Error uploading image to S3",
+        };
+      }
+      return await prisma.step.update({
+        where: {
+          id,
+        },
+        data: {
+          image: {
+            upsert: {
+              create: {
+                url:
+                  (process.env.CDN_URL ??
+                    `https://${process.env.AWS_S3_BUCKET ?? ""}.s3.${
+                      process.env.AWS_REGION
+                    }.amazonaws.com`) +
+                  "/" +
+                  imageName,
+              },
+              update: {
+                url:
+                  (process.env.CDN_URL ??
+                    `https://${process.env.AWS_S3_BUCKET ?? ""}.s3.${
+                      process.env.AWS_REGION
+                    }.amazonaws.com`) +
+                  "/" +
+                  imageName,
+              },
+            },
+          },
+        },
+      });
+    }
   },
   deleteStep: async (
     _ = null,
